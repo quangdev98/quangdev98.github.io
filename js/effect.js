745d5f324b5b40b76ba0bde152e47df3b76a953d
quangdev98 <dvq.dev@gmail.com>
2021-03-03 09:32:10 +0700

    fix_header

--- js/effect.js
+++ js/effect.js
@@ -1,16 +1,27 @@
-$('#sliderScreen').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(document).ready(function() {
+    var i = 0;
+    $(document).on('click', '.slider-next', function(){
+        var next = i + 100;
+        if (i === 70) {
+          $('div.scroll-wrapper ul.wrapSlide li').removeAttr('style');
+          $('div.scroll-wrapper ul.wrapSlide li').attr('style', 'left:0%;-webkit-transition:left 1s;transition:left 1s;');
+          i = 0;
+        } else {
+          $('div.scroll-wrapper ul.wrapSlide li').removeAttr('style');
+          $('div.scroll-wrapper ul.wrapSlide li').attr('style', 'left:-'+ next +'.1%;-webkit-transition:left 1s;transition:left 1s;');
+          i = next;
+        }   
+    });
+    $(document).on('click', '.slider-previous', function(){
+        var previous = i - 100;
+        if (i === 0) {
+            $('div.scroll-wrapper ul.wrapSlide li').removeAttr('style');
+            $('div.scroll-wrapper ul.wrapSlide li').attr('style', 'left:-70%;-webkit-transition:left 1s;transition:left 1s;');
+            i = 70;
+          } else {
+            $('div.scroll-wrapper ul.wrapSlide li').removeAttr('style');
+            $('div.scroll-wrapper ul.wrapSlide li').attr('style', 'left:-'+ previous +'.1%;-webkit-transition:left 1s;transition:left 1s;');
+            i = previous;
+          }
+    });
+});
