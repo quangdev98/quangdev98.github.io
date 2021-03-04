e779a6eb93d633e9ee09cff954b5e24c9f6c5a62
quangdev98 <dvq.dev@gmail.com>
2021-03-04 17:31:09 +0700

    fix/set-automatic-slide

--- js/main.js
+++ js/main.js
@@ -24,3 +24,30 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "grid";
     dots[slideIndex - 1].className += " active";
 }
+// automatic-slide-client
+// automatic-slide-client
+// automatic-slide-client
+var slide = 0;
+var dot = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var y;
+    var x = document.getElementsByClassName("client_");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slide++;
+    if (slide > x.length) { slide = 1 }
+    x[slide - 1].style.display = "grid";
+
+    for (y = 0; y < dots.length; y++) {
+        dots[y].className = dots[y].className.replace("active", "");
+    }
+    dot++;
+     if (dot > dots.length) { dot = 1 }
+    dots[dot - 1].className += " active";
+    setTimeout(carousel, 4000); 
+}
