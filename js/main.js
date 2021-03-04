d423f8b4036b3e2ce79242afc22c63066340e144
quangdev98 <dvq.dev@gmail.com>
2021-03-04 16:56:09 +0700

    fix/slider

--- js/main.js
+++ js/main.js
@@ -1,38 +1,26 @@
-        let i = 0;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-        let box1 = document.getElementById("item-client1");
-        let box2 = document.getElementById("item-client2");
-        let box3 = document.getElementById("item-client3");
-        let dot1 = document.querySelector(".dot1");
-        let dot2 = document.querySelector(".dot2");
-        let dot3 = document.querySelector(".dot3");
-        dot1.onclick = function () {
-            box1.style.opacity = 1;
-            box2.style.opacity = 0;
-            box3.style.opacity = 0;
-            i = 0;
-        }
-        dot2.onclick = function () {
-            box1.style.opacity = 0;
-            box2.style.opacity = 1;
-            box3.style.opacity = 0;
-            i = 1;
-        }
-        dot3.onclick = function () {
-            box1.style.opacity = 0;
-            box2.style.opacity = 0;
-            box3.style.opacity = 1;
-            i = 2;
-        }
-        document.addEventListener("keydown", (e) => {
-            if (e.keyCode == 37) {
-                right();
-            }
-        });
-        document.addEventListener("keydown", (e) => {
-            if (e.keyCode == 39 ) {
-                right();
-            }
-        });
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-       
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("client_");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "grid";
+    dots[slideIndex - 1].className += " active";
+}
